9c098acb0b66065452c5c36ffb22a087edd6356a
Your Name <you@example.com>
2016-05-05 23:33:53 +0800

    restrict inde

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -20,6 +20,37 @@ class SiteController extends Controller
 			),
 		);
 	}
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', 
+			'postOnly + delete',
+		);
+	}
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',
+				'actions'=>array('error','login','logout'),
+				'users'=>array('*'),
+			),
+			array('allow',
+				'actions'=>array('index'),
+				'users'=>array('@'),
+			),
+			array('deny',
+				'users'=>array('*'),
+			),
+		);
+	}
 
 	/**
 	 * This is the default 'index' action that is invoked
